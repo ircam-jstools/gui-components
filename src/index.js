22417a4fc08dab909f1362065b994385d1377958
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2018-10-25 17:19:02 +0200

    do not expose broken components

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
  * @module gui-components
  */
 export { default as Slider } from './Slider';
-export { default as Breakpoint } from './Breakpoint';
-export { default as Matrix } from './Matrix';
+// export { default as Breakpoint } from './Breakpoint';
+// export { default as Matrix } from './Matrix';
